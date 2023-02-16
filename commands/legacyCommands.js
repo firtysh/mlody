954e164fd10842fe87035127f47419c7f8fedc85
SHAIKASIFALI <12006051@svuceedu.onmicrosoft.com>
2023-02-16 22:34:16 +0530

    added currentplaying command

--- commands/legacyCommands.js
+++ commands/legacyCommands.js
@@ -4,35 +4,55 @@ import {
   createAudioPlayer,
   getVoiceConnection,
   VoiceConnectionStatus,
-} from '@discordjs/voice';
-import ytsr from 'ytsr';
-import makeResource from '../utils/makeResource.js';
-import {
-  addSong,
-  nextSong,
-  getQueue,
-} from '../utils/musicQueue.js';
+} from "@discordjs/voice";
+import ytsr from "ytsr";
+import makeResource from "../utils/makeResource.js";
+import { addSong, nextSong, getQueue } from "../utils/musicQueue.js";
 
 export default {
-  ping: {
-    description: 'Ping the bot',
+  currentSong: {
+    description: "to display the song name",
     acceptArgs: false,
     execute: async ({ message }) => {
-      await message.reply('Pong!');
+      if (getQueue({ guild: message.guild.id })) {
+        return await message.reply(
+          getQueue({ guild: message.guild.id }).songs[
+            getQueue({ guild: message.guild.id }).currentSong
+          ].title
+        );
+      } else {
+        await message.reply(
+          "no songs currently playing in queue kindly play the song first"
+        );
+      }
+    },
+  },
+  ping: {
+    description: "Ping the bot",
+    acceptArgs: false,
+    execute: async ({ message }) => {
+      await message.reply("Pong!");
     },
   },
   join: {
-    description: 'Join audio channel',
+    description: "Join audio channel",
     acceptArgs: false,
     execute: async ({ message }) => {
       const voiceConnection = getVoiceConnection(message.guild.id);
       if (message.member.voice.channel) {
-        if (voiceConnection?.joinConfig.channelId === message.member.voice.channel.id) {
-          await message.reply('Already in this voice channel');
+        if (
+          voiceConnection?.joinConfig.channelId ===
+          message.member.voice.channel.id
+        ) {
+          await message.reply("Already in this voice channel");
           return;
         }
-        if (voiceConnection && voiceConnection?.joinConfig.channelId !== message.member.voice.channel.id) {
-          await message.reply('Already in a voice channel');
+        if (
+          voiceConnection &&
+          voiceConnection?.joinConfig.channelId !==
+            message.member.voice.channel.id
+        ) {
+          await message.reply("Already in a voice channel");
           return;
         }
         await message.reply(`Joining ${message.member.voice.channel.name}`);
@@ -48,12 +68,14 @@ export default {
               entersState(connection, VoiceConnectionStatus.Connecting, 5_000),
             ]);
           } catch (error) {
-            if (connection.state.status === VoiceConnectionStatus.Disconnected) {
+            if (
+              connection.state.status === VoiceConnectionStatus.Disconnected
+            ) {
               connection.destroy();
             }
           }
         });
-        connection.on('error', () => {
+        connection.on("error", () => {
           try {
             connection.destroy();
           } catch (error) {
@@ -61,45 +83,53 @@ export default {
           }
         });
       } else {
-        await message.reply('You need to join a voice channel first!');
+        await message.reply("You need to join a voice channel first!");
       }
     },
   },
   leave: {
-    description: 'Leave audio channel',
+    description: "Leave audio channel",
     acceptArgs: false,
     execute: async ({ message }) => {
       const voiceConnection = getVoiceConnection(message.guild.id);
       if (!voiceConnection) {
-        await message.reply('Not in a voice channel');
+        await message.reply("Not in a voice channel");
         return;
       }
-      if (message.member.voice.channel.id === voiceConnection?.joinConfig.channelId) {
+      if (
+        message.member.voice.channel.id ===
+        voiceConnection?.joinConfig.channelId
+      ) {
         await message.reply(`Leaving ${message.member.voice.channel.name}`);
         voiceConnection.destroy();
       } else {
-        await message.reply('You need to join the voice channel first!');
+        await message.reply("You need to join the voice channel first!");
       }
     },
   },
   play: {
-    description: 'Play a song',
+    description: "Play a song",
     acceptArgs: true,
     execute: async ({ message, args }) => {
       // get voice connection by guild id
       const voiceConnection = getVoiceConnection(message.guild.id);
-      if (!voiceConnection) { // if bot is not in a voice channel
-        await message.reply('Not in a voice channel');
+      if (!voiceConnection) {
+        // if bot is not in a voice channel
+        await message.reply("Not in a voice channel");
         return;
       }
-      if (message.member.voice.channel.id !== voiceConnection?.joinConfig.channelId) {
+      if (
+        message.member.voice.channel.id !==
+        voiceConnection?.joinConfig.channelId
+      ) {
         // if user is not in the same voice channel as the bot
-        await message.reply('You need to join the voice channel first!');
+        await message.reply("You need to join the voice channel first!");
         return;
       }
       try {
         // get first result from youtube search
-        const { url, title, duration } = (await ytsr(args, { limit: 1 })).items[0];
+        const { url, title, duration } = (await ytsr(args, { limit: 1 }))
+          .items[0];
 
         // generate song object
         const song = {
@@ -110,30 +140,38 @@ export default {
         };
         if (!getQueue({ guild: message.guild.id })) {
           const player = createAudioPlayer();
-          player.on('stateChange', async (oldState, newState) => {
-            console.log('state chnged from', oldState.status, 'to', newState.status);
-            if (newState.status === 'idle') {
+          player.on("stateChange", async (oldState, newState) => {
+            console.log(
+              "state chnged from",
+              oldState.status,
+              "to",
+              newState.status
+            );
+            if (newState.status === "idle") {
               const next = nextSong({ guild: message.guild.id });
               if (next) {
                 player.play(makeResource(next.url));
                 await message.reply(`Playing ${next.title}`);
               } else {
-                await message.reply('No more songs in queue');
+                await message.reply("No more songs in queue");
               }
             }
           });
           addSong({ guild: message.guild.id, song, player });
-          voiceConnection.subscribe(getQueue({ guild: message.guild.id }).player);
+          voiceConnection.subscribe(
+            getQueue({ guild: message.guild.id }).player
+          );
           await message.reply(`Playing ${title}`);
           player.play(makeResource(url));
         } else {
           addSong({ guild: message.guild.id, song });
-          message.reply('Song added to queue');
+          message.reply("Song added to queue");
         }
       } catch (error) {
         console.log(error);
-        await message.reply('Could not play song');
+        await message.reply("Could not play song");
       }
     },
   },
+  // this command displays the song that is currently playing
 };
--- utils/parseMessage.js
+++ utils/parseMessage.js
@@ -1,10 +1,14 @@
-import { config } from 'dotenv';
+import { config } from "dotenv";
 
 config();
 
 export default (message) => {
   if (message.startsWith(process.env.COMMAND_PREFIX)) {
-    const [command, args] = message.trim().substring(process.env.COMMAND_PREFIX.length).split(/(?<=^\S+)\s+/);
+    const [command, args] = message
+      .trim()
+      .substring(process.env.COMMAND_PREFIX.length)
+      .split(" ");
+    console.log(command);
     return { command, args };
   }
   return null;
